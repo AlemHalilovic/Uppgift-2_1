c59de751b65e67dfe1a0bd5f5bf7ead08b7d2099
Admin <Admin@TEB123009>
2022-09-14 11:28:06 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favorit bok?");
+            string FavoritBok = Console.ReadLine();
+            Console.WriteLine("Låter som en bra bok!");
+           
+         
+        }
+    }
+}
